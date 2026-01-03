4a756e5914c9a603519194c221ea5a17e42cf6be
Patrick <patrickrebecchi@hotmail.com>
2026-01-03 20:49:07 -0300

    fix: Melhorando o main do projeto

--- src/main/java/principal/PrincipalBrasilApi.java
+++ src/main/java/principal/PrincipalBrasilApi.java
@@ -6,26 +6,37 @@ import service.BrasilApiService;
 import java.util.Scanner;
 
 public class PrincipalBrasilApi {
+
     public static void main(String[] args) {
 
         Scanner scanner = new Scanner(System.in);
         BrasilApiService service = new BrasilApiService();
 
-        System.out.println("Boa tarde, qual o codigo do banco.");
-        String codigoBanco = scanner.nextLine();
+        while (true) {
+            System.out.println("Digite o código do banco (ou 0 para sair):");
+            String codigoBanco = scanner.nextLine();
 
-        if (codigoBanco.isBlank()) {
-            System.out.println("Código do banco não pode ser vazio.");
-            return;
+            if (codigoBanco.equals("0")) {
+                System.out.println("Encerrando aplicação...");
+                break;
+            }
+
+            if (codigoBanco.isBlank()) {
+                System.out.println("Código do banco não pode ser vazio.");
+                continue; // volta para o início do loop
+            }
+
+            Banco banco = service.buscarBanco(codigoBanco);
+
+            if (banco == null) {
+                System.out.println("Banco inválido ou não encontrado.");
+            } else {
+                System.out.println(banco);
+            }
+
+            System.out.println();
         }
 
-        Banco banco = service.buscarBanco(codigoBanco);
-
-        if (banco == null) {
-            System.out.println("Banco inválido ou não encontrado.");
-        } else {
-            System.out.println(banco);
-        }
 
         scanner.close();
     }
